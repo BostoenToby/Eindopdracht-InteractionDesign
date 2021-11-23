c9d0061dc5a699d36cc8b5a85cc72b9659c53cf3
Toby Bostoen <toby.bostoen@student.howest.be>
2021-11-23 21:32:44 +0100

    range in days + calculator + info chart

--- script/app.js
+++ script/app.js
@@ -1,4 +1,9 @@
-let JsonTest, screenChart;
+let JsonObject,
+  screenChart,
+  totalDays,
+  currentValue,
+  chosenDays = 30,
+  chosenCoin = 'USD';
 
 const myChart = function (title, labels, data) {
   let myChart = document.getElementById('myChart').getContext('2d');
@@ -13,48 +18,74 @@ const myChart = function (title, labels, data) {
         },
       ],
     },
-    options: {},
+    options: {
+      maintainAspectRatio: false,
+    },
   });
-  console.log("hallo")
 };
 
 const ChooseExchange = function () {
-  let showExchange = document.querySelector('.js-showChartAndInfo');
-  console.log(showExchange)
-  showExchange.addEventListener('click', function () {
-    const coin = document.querySelector('.js-choose-currency').value;
-    showExhangeRate(JsonTest, coin)
+  document.querySelector('.js-showChartAndInfo').addEventListener('click', function () {
+    chosenCoin = document.querySelector('.js-choose-currency').value;
+    showExhangeRate();
+  });
+
+  document.querySelector('.js-calculate-original').addEventListener('change', function () {
+    document.querySelector('.js-calculate-result').value = document.querySelector('.js-calculate-original').value * currentValue;
+  });
+
+  document.querySelector('.js-calculate-result').addEventListener('change', function () {
+    document.querySelector('.js-calculate-original').value = document.querySelector('.js-calculate-result').value / currentValue;
+  });
+
+  document.querySelector('.js-range-days').addEventListener('change', function () {
+    console.log('eerste test');
+    chosenDays = document.querySelector('.js-range-days').value;
+    showExhangeRate();
   });
 };
 
 const readJson = function (JsonData) {
-  const JsonObject = JSON.parse(JsonData);
-  JsonTest = JsonObject;
+  const Json = JSON.parse(JsonData);
+  JsonObject = Json;
   showExchangeValues(JsonObject);
 };
 
-const showExhangeRate = function (JsonObject, coin) {
-  var data = [];
-  var labels = [];
-  const title = `Exchange rate ${coin}`;
-  const Json = JsonObject.data;
-  let total = 0;
-  for (let item in Json) {
-    total += 1;
-  }
-  let coinString = coin;
-  counter = total - 30;
-  while (counter < total) {
-    let dayExchange = Json[counter];
-    const dateExhange = dayExchange.date;
-    const coinExchange = dayExchange.rates[coin];
-    data.push(coinExchange);
-    labels.push(dateExhange);
+const showMaxExhangeStats = function (range) {
+  counter = totalDays - range;
+  let maxValue = 0;
+  while (counter < totalDays) {
+    if (JsonObject.data[counter].rates[chosenCoin] > maxValue) {
+      maxValue = JsonObject.data[counter].rates[chosenCoin];
+    }
     counter += 1;
   }
-  console.log(data);
-  console.log(labels);
-  if (screenChart != null){
+  return maxValue;
+};
+
+const showExhangeRate = function () {
+  console.log('test');
+  var data = [];
+  var labels = [];
+  const title = `Exchange rate ${chosenCoin}`;
+  totalDays = 0;
+  for (let item in JsonObject.data) {
+    totalDays += 1;
+  }
+  counter = totalDays - chosenDays;
+  //get data for chart
+  while (counter < totalDays) {
+    data.push(JsonObject.data[counter].rates[chosenCoin]);
+    labels.push(JsonObject.data[counter].date);
+    counter += 1;
+  }
+  currentValue = JsonObject.data[totalDays - 1].rates[chosenCoin];
+  document.querySelector('.js-max-month').innerHTML = showMaxExhangeStats(30, chosenCoin);
+  document.querySelector('.js-max-year').innerHTML = showMaxExhangeStats(365, chosenCoin);
+  document.querySelector('.js-current-value').innerHTML = currentValue;
+  document.querySelector('.js-calculating-coin').innerHTML = chosenCoin;
+
+  if (screenChart != null) {
     screenChart.destroy();
   }
   myChart(title, labels, data);
@@ -62,17 +93,15 @@ const showExhangeRate = function (JsonObject, coin) {
 
 const showExchangeValues = function (JsonObject) {
   let HtmlString = '';
-  const exchangeRatesPlaceholder = document.querySelector('.js-choose-currency');
-  let exchangeDay = JsonObject.data[0];
-  for (let exchange in exchangeDay.rates) {
+  for (let exchange in JsonObject.data[0].rates) {
     if (exchange == 'USD') {
       HtmlString += `<option value="${exchange}" selected>${exchange}</option>`;
     } else {
       HtmlString += `<option value="${exchange}">${exchange}</option>`;
     }
   }
-  exchangeRatesPlaceholder.innerHTML = HtmlString;
-  showExhangeRate(JsonObject, 'USD');
+  document.querySelector('.js-choose-currency').innerHTML = HtmlString;
+  showExhangeRate();
   ChooseExchange();
 };
 
